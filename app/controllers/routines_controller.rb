57ddf1447194830eccaf3e42a344f520c1cd1b73
Vanigami <39018244+Vanigami@users.noreply.github.com>
2020-01-08 19:39:32 -0500

    adds nested index.erb file for routinescontroller

---
+++ app/controllers/routines_controller.rb
@@ -0,0 +1,33 @@
+class RoutinesController < ApplicationController
+    # CREATE 
+
+        # New <= to render the form
+        # make a get request to '/routines/new'
+
+        # Create
+        # make a post request to '/routines'
+
+
+    #READ 
+
+        # Index 
+        #make a get request to '/routines' <=static route
+        
+        get '/routines' do 
+            erb :'routines/index'
+        end
+        # Show 
+        # make a get request to '/routines/:id' <= dynamic route
+
+    #UPDATE
+
+        # Edit 
+        # make a get request to '/routines/:id/edit'
+        # Update
+        # make a patch request to '/routines/:id'
+
+    #DESTROY
+        
+        # make a delete request to '/routines/:id'
+
+end
