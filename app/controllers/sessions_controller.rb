2b35b838c02d941311518ba3ce6d3f7e828ed57f
Vanigami <39018244+Vanigami@users.noreply.github.com>
2020-01-12 16:18:32 -0500

    creates sessions controller actions for user login

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,27 @@
+class SessionsController < ApplicationController
+
+    #user can log in 
+    get '/login' do 
+        erb :'users/login'
+    end
+
+    post '/login' do         
+        if params[:username].empty? || params[:password].empty?
+            @error = "Username or password can't be blank"
+            erb :'users/login'
+        else
+            if user = User.find(username: params[:username], password: params[:password])
+                session[:user_id] = user.id
+                redirect '/routines'
+            else
+                @error = "Account not found"
+                erb :'/users/login'
+            end
+        end
+    end
+
+
+
+    #user can log out 
+
+end
